82ceb2ec6cdc0f4345c5a1bfb58b9547fde10506
mayeths <wow@mayeths.com>
2020-10-23 09:35:16 +0800

    Refactor add_masm32_to_env comments

--- script/add_masm32_to_env.cmd
+++ script/add_masm32_to_env.cmd
@@ -2,20 +2,20 @@
 :: https://stackoverflow.com/a/15885133
 
 @echo off
+
+
 setlocal
-
 echo Please select the folder of MASM32
-
 set psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please select the folder of MASM32.',0,0).self.path"
-
 for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
+
 
 setlocal enabledelayedexpansion
 echo.
 echo Set user enviroment variable: %%MASM32%%=!folder!
 setx MASM32 "!folder!"
 echo.
-echo Please close Visual Studio 2019 and rerun the projects, thank you.
+echo Please RESTART Visual Studio 2019 and rebuild the projects, thank you.
 echo.
 endlocal
 
