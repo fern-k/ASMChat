ab9d938bbad851a48bb63f74586db3db726ca895
mayeths <wow@mayeths.com>
2020-10-25 10:15:12 +0800

    Refactor add_masm32_to_env.cmd

--- script/add_masm32_to_env.cmd
+++ script/add_masm32_to_env.cmd
@@ -1,22 +1,17 @@
-:: File / folder chooser dialog from a Windows batch script
-:: https://stackoverflow.com/a/15885133
-
 @echo off
 
 
-setlocal
+:: File / folder chooser dialog from a Windows batch script
+:: https://stackoverflow.com/a/15885133
 echo Please select the folder of MASM32
-set psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please select the folder of MASM32.',0,0).self.path"
-for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
+set openFolderDialogCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Please select the folder of MASM32.',0,0).self.path"
+for /f "usebackq delims=" %%I in (`powershell %openFolderDialogCommand%`) do set "folder=%%I"
 
 
 setlocal enabledelayedexpansion
-echo.
 echo Set user enviroment variable: %%MASM32%%=!folder!
 setx MASM32 "!folder!"
-echo.
 echo Please RESTART Visual Studio 2019 and rebuild the projects, thank you.
-echo.
 endlocal
 
 pause
